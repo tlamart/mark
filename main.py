1dd2169885ec8615a7fdabfcea00e0bd2926ec43
tlamart <tlamart@outlook.com>
2026-08-12 18:28:10 +0200

    add MVP's routes

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def root():
     return({"message":"hello world!"})
+
+@app.get("/books")
+def get_books():
+    return []
+# TODOUM connect to SQLite and return list of every book
+
+@app.post("/books")
+def add_book():
+    pass
+# TODOUM
+
+@app.patch("/books/{id}")
+def update_book_id(id :int):
+    pass
+# TODOUM
